68b963948686e0b08bb7f568ed0eec81a291ab3c
heyuimd <heyuimd@gmail.com>
2020-11-22 16:30:30 +0900

    Init

---
+++ src/main/java/kakaopay/money_sprinkle/MoneySprinkleApplication.java
@@ -0,0 +1,13 @@
+package kakaopay.money_sprinkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneySprinkleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneySprinkleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kakaopay/money_sprinkle/MoneySprinkleApplicationTests.java
@@ -0,0 +1,13 @@
+package kakaopay.money_sprinkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneySprinkleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
